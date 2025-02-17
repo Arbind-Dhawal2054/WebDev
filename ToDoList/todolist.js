423f0dcb6fc00f7cc4f208721f625010679baf22
Arbind Kumar Dhawal <arbinddhawal@gmail.com>
2025-02-17 14:21:36 +0000

    Implemented displayResult function to show the added todo items

--- ToDoList/todolist.js
+++ ToDoList/todolist.js
@@ -1,5 +1,33 @@
+let todoItems=[];
+
+displayTodo();
+
+
 function todoInput()
 {
     let takeInput= document.querySelector('#input-box');
-    let inputValue= takeInput.value;
+    let inputValue= takeInput.value.trim();
+    if(inputValue!=='')
+    {
+        todoItems.push(inputValue);
+        takeInput.value='';
+
+        displayTodo();
+    }
 }
+
+function displayTodo()
+{
+    let displayItems=document.querySelector('#todo-list');
+    displayItems.innerText='';
+    for(let i=0;i<todoItems.length; i++)
+    {
+        displayItems.innerText=displayItems.innerText+ '\n'+todoItems[i];
+    }
+}
+
+document.querySelector('#input-box').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        todoInput();
+    }
+});

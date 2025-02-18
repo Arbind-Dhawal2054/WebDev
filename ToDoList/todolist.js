26a4d0cb90c4469349e669bc1b356e577f903523
Arbind Kumar Dhawal <arbinddhawal@gmail.com>
2025-02-18 12:01:58 +0000

    Added Date and itsfunctionality accordigly

--- ToDoList/todolist.js
+++ ToDoList/todolist.js
@@ -7,23 +7,48 @@ function todoInput()
 {
     let takeInput= document.querySelector('#input-box');
     let inputValue= takeInput.value.trim();
-    if(inputValue!=='')
+    let takeDate= document.querySelector('#date-items');
+    let dateValue= takeDate.value;
+    if(inputValue==='' || dateValue==='')
     {
-        todoItems.push(inputValue);
+        alert("Either Todo or Date is missing!"); // Prevent adding without a date and Todo
+        return;
+    }
+
+        todoItems.push({item:inputValue, duedate:dateValue});
+
         takeInput.value='';
+        takeDate.value='';
 
         displayTodo();
-    }
+    
 }
 
 function displayTodo()
 {
-    let displayItems=document.querySelector('#todo-list');
-    displayItems.innerText='';
+    let newHTML='';
+    let displayItems=document.querySelector('.todo-list');
+    displayItems.innerHTML='';
     for(let i=0;i<todoItems.length; i++)
     {
-        displayItems.innerText=displayItems.innerText+ '\n'+todoItems[i];
+        let {item, duedate}= todoItems[i];
+    
+       newHTML+=`
+       <div>
+       <span>${item} </span>
+       <span>${duedate}</span>
+       <button onclick="deleteTodo(${i})">Delete
+       </button>
+       </div>
+       `;
     }
+    displayItems.innerHTML=newHTML;
+}
+
+function deleteTodo(index)
+{
+    todoItems.splice(index, 1);
+    displayTodo();
 }
 
 document.querySelector('#input-box').addEventListener('keydown', function(event) {
